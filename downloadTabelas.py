f126b08d182e8873e6d83652a2d5941ff664dc45
matheushmoura <matheusshutt2@gmail.com>
2022-01-17 23:53:39 -0300

    Correções

--- downloadTabelas.py
+++ downloadTabelas.py
@@ -6,7 +6,7 @@ import time
 encoding = "utf-8"
 
 def downloadTabela():
-    print('downloadTabela')
+    #print('downloadTabela')
     driver = Browserconfig.browser(True)
 
     link = "https://repositorio.seade.gov.br/dataset/pib-municipal-2002-2018"
@@ -16,7 +16,7 @@ def downloadTabela():
     for x in range(1, len(contador)):
         titulo = driver.find_element(By.CSS_SELECTOR, "#dataset-resources > ul > li:nth-child(" + str(x) + ") > a").text
         if "Tabela - PIB" in titulo:
-            print(titulo)
+            #print(titulo)
             driver.find_element(By.CSS_SELECTOR, "#dataset-resources > ul > li:nth-child(" + str(x) + ") > div > a").click()
             WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#dataset-resources > ul > li:nth-child(" + str(x) + ") > div > ul > li:nth-child(2) > a")))
             driver.find_element(By.CSS_SELECTOR, "#dataset-resources > ul > li:nth-child(" + str(x) + ") > div > ul > li:nth-child(2) > a").click()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from downloadTabelas import downloadTabela
 import os
 
-datasp = pd.DataFrame(columns=['ano', 'sp', 'pp', 'g', 'c'])
+datasp = pd.DataFrame(columns=['ano', 'sp', 'sbc', 'g', 'c', 'sjc'])
 # Reading data downloaded in'seade.gov.br'
 downloadTabela()
 # Plotting
@@ -14,18 +14,19 @@ for file in glob.glob("*.xlsx"):
     filesplit = file.split('pib_')[1]
     filesplit2 = filesplit.split('.')[0]
     year = filesplit2.split('-')[0]
-    #print(year)
+    print(year)
 
     df = pd.read_excel(file, header=10)
     os.remove(file)
     df.columns = ['MUNICIPIO', 'AGROPECUARIA', 'INDUSTRIA', 'ADM PUBLICA', 'SUBTOTAL', 'TOTAL', 'IMPOSTOS', 'PIB', 'PIB CAPITA']
-    #print(df)
+    print(df)
     datasp = datasp.append({
         'ano': str(year),
-        'sp': df.loc[df['MUNICIPIO'] == "São Paulo"]['PIB CAPITA'].values * 1000,
-        'pp': df.loc[df['MUNICIPIO'] == "Presidente Prudente"]['PIB CAPITA'].values * 1000,
-        'g': df.loc[df['MUNICIPIO'] == "Guarulhos"]['PIB CAPITA'].values * 1000,
-        'c': df.loc[df['MUNICIPIO'] == "Campinas"]['PIB CAPITA'].values * 1000
+        'sp': df.loc[df['MUNICIPIO'] == "São Paulo"]['PIB CAPITA'].values,
+        'sbc': df.loc[df['MUNICIPIO'] == "São Bernardo do Campo"]['PIB CAPITA'].values,
+        'g': df.loc[df['MUNICIPIO'] == "Guarulhos"]['PIB CAPITA'].values,
+        'c': df.loc[df['MUNICIPIO'] == "Campinas"]['PIB CAPITA'].values,
+        'sjc': df.loc[df['MUNICIPIO'] == "São José dos Campos"]['PIB CAPITA'].values
     }, ignore_index=True)
 
 print(datasp)
@@ -34,16 +35,19 @@ print(datasp)
 ax = plt.gca()
 ax.set_facecolor('#F6F9ED')
 datasp['sp'] = datasp['sp'].astype(float)
-datasp['pp'] = datasp['pp'].astype(float)
+datasp['sbc'] = datasp['sbc'].astype(float)
 datasp['g'] = datasp['g'].astype(float)
 datasp['c'] = datasp['c'].astype(float)
+datasp['sjc'] = datasp['sjc'].astype(float)
+
 datasp.plot(kind='line', x='ano', y='sp', label='São Paulo (SP)', ax=ax, color='#42124C')
-datasp.plot(kind='line', x='ano', y='c', label='Campinas (SP)', ax=ax, color='#FE0472')
-datasp.plot(kind='line', x='ano', y='g', label='Guarulhos (SP)', ax=ax, color='#8AFF00')
-datasp.plot(kind='line', x='ano', y='pp', label='Presidente Prudente (SP)', ax=ax, color='#C3FF13')
+datasp.plot(kind='line', x='ano', y='c', label='Campinas (SP)', ax=ax, color='#2FF3E0')
+datasp.plot(kind='line', x='ano', y='g', label='Guarulhos (SP)', ax=ax, color='#F8D210')
+datasp.plot(kind='line', x='ano', y='sbc', label='São Bernardo do Campo (SP)', ax=ax, color='#FA26A0')
+datasp.plot(kind='line', x='ano', y='sjc', label='São José dos Campos (SP)', ax=ax, color='#F51720')
 
 ax.grid(False)
-plt.title(' Gross Domestic Product (GDP) \n Of the 5 largest cities in São Paulo (State)', fontsize=14, alpha=0.8)
+plt.title(' Gross Domestic Product (GDP) \n of the 5 largest cities in São Paulo (State)', fontsize=14, alpha=0.8)
 plt.ylabel('GDP Per Capita in Real(R$)', fontsize=13, alpha=0.8)
 plt.xlabel('Year', fontsize=13, alpha=0.8)
 plt.tick_params(top='off', right='off', labelbottom='on')
